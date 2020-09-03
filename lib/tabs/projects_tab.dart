fcfe488d655b9f5e1fcad4a70d66721e309deb61
Yash Khare <yashsja@gmail.com>
2020-09-03 20:28:44 +0530

    Add hover effect to projects section

--- lib/tabs/projects_tab.dart
+++ lib/tabs/projects_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/config/projects.dart';
 import 'package:portfolio/widgets/project_widget.dart';
 import 'package:portfolio/widgets/responsive_widget.dart';
+import 'package:hover_effect/hover_effect.dart';
 
 class ProjectsTab extends StatelessWidget {
   @override
@@ -13,7 +14,23 @@ class ProjectsTab extends StatelessWidget {
         childAspectRatio: MediaQuery.of(context).size.width /
             (MediaQuery.of(context).size.height / 1.3),
         children: List.generate(
-            projects.length, (index) => ProjectWidget(projects[index], 0)),
+          projects.length,
+          (index) => Container(
+            color: Colors.white,
+            child: HoverCard(
+              builder: (context, hovering) {
+                return ProjectWidget(projects[index], 0);
+              },
+              depth: 20,
+              depthColor: Colors.white,
+              shadow: BoxShadow(
+                  color: Colors.cyanAccent,
+                  blurRadius: 40,
+                  spreadRadius: -30,
+                  offset: Offset(0, 60)),
+            ),
+          ),
+        ),
       ),
       smallScreen: ListView.builder(
           itemCount: projects.length,
